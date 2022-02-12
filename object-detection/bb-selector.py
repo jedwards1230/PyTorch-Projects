72a53708bf96267ee55c2b216bcd768d1bf55110
jedwards1230 <justinedwards1230@gmail.com>
2022-02-12 14:34:55 -0500

    cleaned and commented

--- object-detection/bb-selector.py
+++ object-detection/bb-selector.py
@@ -1,67 +1,50 @@
 from imutils.video import FPS
 import cv2
 
-OPENCV_OBJECT_TRACKERS = {
-    "csrt": cv2.legacy.TrackerCSRT_create,
-    "kcf": cv2.TrackerKCF_create,
-    "mil": cv2.TrackerMIL_create
-}
-
 video_stream = "http://192.168.1.143:56000/mjpeg"
 
 vcap = cv2.VideoCapture(video_stream)
+
 if not vcap.isOpened():
     print("Cannot open camera")
     exit()
+    
 vcap.set(3, 640)
 vcap.set(4, 480)
-tracker_choice = 'kcf'
 
-tracker = OPENCV_OBJECT_TRACKERS[tracker_choice]()
+tracker = cv2.TrackerKCF_create()
 initBB = None
 fps = None
 
 while True:
     ret, img = vcap.read()
-    if not ret:
-        continue
-    (H, W) = img.shape[:2]
-    if initBB is not None:
-        # grab the new bounding box coordinates of the object
-        (success, box) = tracker.update(img)
-        # check to see if the tracking was a success
-        if success:
-            (x, y, w, h) = [int(v) for v in box]
-            cv2.rectangle(img, (x, y), (x + w, y + h),
-                (0, 255, 0), 2)
-        # update the FPS counter
-        fps.update()
-        fps.stop()
-        # initialize the set of information we'll be displaying on
-        # the frame
-        info = [
-            ("Tracker", tracker_choice),
-            ("Success", "Yes" if success else "No"),
-            ("FPS", "{:.2f}".format(fps.fps())),
-        ]
-        # loop over the info tuples and draw them on our frame
-        for (i, (k, v)) in enumerate(info):
-            text = "{}: {}".format(k, v)
-            cv2.putText(img, text, (10, H - ((i * 20) + 20)),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    if img is not None:
+        (H, W) = img.shape[:2]
+        if initBB is not None:
+            (success, box) = tracker.update(img)
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(img, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2)
+                
+            fps.update()
+            fps.stop()
             
-    cv2.imshow("Frame", img)
-    key = cv2.waitKey(1) & 0xFF
-    # if the 's' key is selected, we are going to "select" a bounding
-    # box to track
-    if key == ord("k"):
-        # select the bounding box of the object we want to track (make
-        # sure you press ENTER or SPACE after selecting the ROI)
-        initBB = cv2.selectROI("Frame", img, fromCenter=False,
-            showCrosshair=True)
-        # start OpenCV object tracker using the supplied bounding box
-        # coordinates, then start the FPS throughput estimator as well
-        tracker.init(img, initBB)
-        fps = FPS().start()
-    elif key == ord("q"):
-        break
+            info = [
+                ("Success", "Yes" if success else "No"),
+                ("FPS", "{:.2f}".format(fps.fps())),
+            ]
+            # loop over the info tuples and draw them on our frame
+            for (i, (k, v)) in enumerate(info):
+                text = f'{k}: {v}'
+                cv2.putText(img, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                
+        cv2.imshow("Frame", img)
+        key = cv2.waitKey(1) & 0xFF
+        
+        if key == ord("k"):
+            initBB = cv2.selectROI("Frame", img, fromCenter=False, showCrosshair=True)
+            tracker.init(img, initBB)
+            fps = FPS().start()
+        elif key == ord("q"):
+            break
--- object-detection/torch-model.py
+++ object-detection/torch-model.py
@@ -1,5 +1,6 @@
 from torchvision import models
 from torchvision import transforms as T
+from imutils.video import FPS
 import torch
 import numpy as np
 import cv2
@@ -8,6 +9,9 @@ import time
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 video_stream = "http://192.168.1.143:56000/mjpeg"
+
+box_color = [0, 255, 0]
+fps = None
 
 coco_classes = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
@@ -25,63 +29,71 @@ coco_classes = [
 ]
 
 class MobileNetDetection:
-    def __init__(self):
+    def __init__(self, threshold=0.5):
+        self.threshold = threshold
         self.model = models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True, pretrained_backbone=True)
-        #self.model = models.detection.retinanet_resnet50_fpn(pretrained=True, pretrained_backbone=True)
         self.model.eval()
 
-    def get_prediction(self, img, threshold=0.5):
+    def get_prediction(self, img):
+        # Process frame through model
         transform = T.Compose([T.ToTensor()])
         img = transform(img)
-        
         pred = self.model([img])
+        
+        # Pull relevant data
         scores = list(pred[0]['scores'].detach().numpy())
         boxes = list(pred[0]['boxes'].detach().numpy())
         labels = list(pred[0]['labels'].numpy())
         
-        if len(scores) > 0 and max(scores) > threshold:
-            pred_tensor = [scores.index(x) for x in scores if x > threshold][-1]
+        # Filter for objects that meet confidence threshold 
+        if len(scores) > 0 and max(scores) > self.threshold:
+            pred_tensor = [scores.index(x) for x in scores if x > self.threshold][-1]
             
-            predicted_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in boxes]
+            predicted_boxes = [[int(i[0]), int(i[1]), int(i[2]), int(i[3])] for i in boxes]
             predicted_boxes = predicted_boxes[:pred_tensor + 1]
             
             predicted_classes = [coco_classes[i] for i in labels]
             predicted_classes = predicted_classes[:pred_tensor + 1]
             
-            return predicted_boxes, predicted_classes
+            predicted_scores = scores[:pred_tensor + 1]
+            
+            return predicted_boxes, predicted_classes, predicted_scores
         else:
             return None, None
 
-    def object_detection(self, img, threshold=0.5, rect_th=3, text_size=3, text_th=3):
-        boxes, classes = self.get_prediction(img, threshold=threshold)
+    def object_detection(self, img):
+        fps = FPS().start()
+        boxes, classes, scores = self.get_prediction(img)
         
         if boxes is not None:
+            fps.update()
+            fps.stop()
+            # draw rectangles and labels
             for i in range(len(boxes)):
-                r, g, b = (0, 255, 0)
-                cv2.rectangle(img, boxes[i][0], boxes[i][1], color=(r, g, b), thickness=rect_th)
-                text = classes[i].upper()
-                cv2.putText(img, text, boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (r, g, b), thickness=text_th)
-            
+                cv2.rectangle(img, (boxes[i][0], boxes[i][1]), (boxes[i][2], boxes[i][3]), color=box_color, thickness=3)
+                text = classes[i].upper() + ' Acc: ' + str(round(scores[i]*100)) + '%'
+                cv2.putText(img, text, (boxes[i][0], boxes[i][1]), cv2.FONT_HERSHEY_SIMPLEX, 2, box_color, thickness=3)
+        
+        # display fps counter
+        fps_text = 'FPS: ' + str(round(fps.fps(), 1))
+        cv2.putText(img, fps_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, box_color, thickness=3)
         cv2.imshow('Output', img)
-        cv2.waitKey(1)
 
 def main():
     mobile_net_detection = MobileNetDetection()
-        
     vcap = cv2.VideoCapture(video_stream)
-    if vcap.isOpened():
-        W = vcap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        H = vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
-        vcap.set(cv2.CAP_PROP_FRAME_WIDTH, W/3)
-        vcap.set(cv2.CAP_PROP_FRAME_HEIGHT, H/3)
-        
-        print(vcap.get(cv2.CAP_PROP_FRAME_WIDTH), vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    if not vcap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-        while True:
-            _, img = vcap.read()
-            if img is not None:
-                mobile_net_detection.object_detection(img)
+    while True:
+        _, img = vcap.read()
+        if img is not None:
+            mobile_net_detection.object_detection(img)
+        key = cv2.waitKey(1)
+        if key is ord('q'):
+            break
         
 if __name__ == "__main__":
     main()
